40a975decbf9f08ad5cb57d74a8a875e462dec43
Gabriel Volpe <volpegabriel@gmail.com>
2021-09-13 17:55:28 +0200

    tidy up

--- default.nix
+++ default.nix
@@ -6,49 +6,58 @@ let
       sha256 = "1rjvbycd8dkkflal8qysi9d571xmgqq46py3nx0wvbzwbkvzf7aw";
     }
   ) {};
-
-  libpath = pkgs.lib.makeLibraryPath (with pkgs; [ stdenv.cc.cc stdenv.cc.libc SDL SDL_gfx SDL_image SDL_ttf ]);
 in
-pkgs.stdenv.mkDerivation {
-  name = "truco";
+  with pkgs;
 
-  src = ./source;
+  stdenv.mkDerivation rec {
+    name = "truco";
 
-  buildPhase = ''
-    g++ main.cpp -o truco-exe `${pkgs.SDL_sixel}/bin/sdl-config --libs --cflags` -lSDLmain -lSDL_gfx -lSDL_image -lSDL_ttf
-  '';
+    src = ./source;
 
-  nativeBuildInputs = with pkgs; [
-    gpp
-  ];
+    buildPhase = ''
+      g++ main.cpp -o truco-exe `${SDL_sixel}/bin/sdl-config --libs --cflags` -lSDLmain -lSDL_gfx -lSDL_image -lSDL_ttf
+    '';
 
-  buildInputs = with pkgs; [
-    freetype
-    libpng12
-    SDL
-    SDL_gfx
-    SDL_image
-    SDL_ttf
-    zlib
-  ];
+    nativeBuildInputs = [
+      gpp
+    ];
 
-  installPhase = ''
-    mkdir -p $out/bin/img
-    cp -r img/* $out/bin/img
-    cp -r ./*.doc $out/bin/
-    cp -r ./*.ttf $out/bin/
-    patchelf \
-      --set-interpreter $(cat ${pkgs.stdenv.cc}/nix-support/dynamic-linker) \
-      --set-rpath "${libpath}" \
-      truco-exe
-    cp truco-exe $out/bin/truco
-  '';
+    buildInputs = [
+      freetype
+      libpng12
+      SDL
+      SDL_gfx
+      SDL_image
+      SDL_ttf
+      zlib
+    ];
 
-  meta = with pkgs.stdenv.lib; {
-    description = "Truco Argentino";
-    homepage    = "https://github.com/gvolpe/truco-argentino";
-    license     = licenses.gpl3;
-    platforms   = platforms.linux;
-    maintainers = [ maintainers.gvolpe ];
-  };
-}
+    libpath = lib.makeLibraryPath [
+      stdenv.cc.cc
+      stdenv.cc.libc
+      SDL
+      SDL_gfx
+      SDL_image
+      SDL_ttf
+    ];
+
+    installPhase = ''
+      mkdir -p $out/bin/img
+      cp -r img/* $out/bin/img
+      cp -r ./*.doc $out/bin/
+      cp -r ./*.ttf $out/bin/
+      patchelf \
+        --set-interpreter $(cat ${stdenv.cc}/nix-support/dynamic-linker) \
+        --set-rpath "${libpath}" \
+        truco-exe
+      cp truco-exe $out/bin/truco
+    '';
+
+    meta = with stdenv.lib; {
+      description = "Truco Argentino";
+      homepage = "https://github.com/gvolpe/truco-argentino";
+      license = licenses.gpl3;
+      platforms = platforms.linux;
+      maintainers = [ maintainers.gvolpe ];
+    };
+  }
